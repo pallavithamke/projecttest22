0ceb4c3011680d5444f3d1dbdb171e7a0e5f82f5
Pallavi Thamke <pallavithamke@gmail.com>
2017-03-15 17:52:11 +0530

    Initial commit

---
+++ js/user.js
@@ -0,0 +1,53 @@
+(function() {
+    "use strict";
+
+    var app = angular.module("myapp", ["ui.router", "ngTable"]);
+    app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/tablepage');
+        $stateProvider
+            .state('tablepage', {
+                url: '/tablepage',
+                templateUrl: 'views/tablepage.html',
+                controller: 'demoController'
+            })
+
+    }]);
+
+    app.controller("demoController", demoController);
+    debugger;
+
+    demoController.$inject = ["NgTableParams"];
+
+    function demoController(NgTableParams) {
+        debugger;
+        this.simpleList =  [
+            { name: "Madhav Sai", age: 10, location: 'Nagpur' },
+            { name: "Suresh Dasari", age: 30, location: 'Chennai' },
+            { name: "Rohini Alavala", age: 29, location: 'Chennai' },
+            { name: "Praveen Kumar", age: 25, location: 'Bangalore' },
+            { name: "Sateesh Chandra", age: 27, location: 'Vizag' },
+            { name: "Siva Prasad", age: 38, location: 'Nagpur' },
+            { name: "Sudheer Rayana", age: 25, location: 'Kakinada' },
+            { name: "Honey Yemineni", age: 7, location: 'Nagpur' },
+            { name: "Mahendra Dasari", age: 22, location: 'Vijayawada' },
+            { name: "Mahesh Dasari", age: 23, location: 'California' },
+            { name: "Nagaraju Dasari", age: 34, location: 'Atlanta' },
+            { name: "Gopi Krishna", age: 29, location: 'Repalle' },
+            { name: "Sudheer Uppala", age: 19, location: 'Guntur' },
+            { name: "Sushmita", age: 27, location: 'Vizag' }
+        ];
+
+
+        this.tableParams = new NgTableParams({
+
+            // initial sort order
+            sorting: { name: "asc" }
+        }, {
+            dataset: this.simpleList
+        });
+    }
+
+
+
+})();
+
